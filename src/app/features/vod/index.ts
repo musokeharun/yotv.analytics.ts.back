f0fed545db49e1607325a07ff71e92fb0283fa11
admin <musokeharun@gmail.com>
2021-08-19 05:56:53 +0300

    Initial On LTS IIIII| No Delayed Response

--- src/app/features/vod/index.ts
+++ src/app/features/vod/index.ts
@@ -1,7 +1,5 @@
 import {response, Router} from "express";
 import {DateTime} from "luxon";
-// @ts-ignore
-import DelayedResponse from "http-delayed-response"
 import {processSql, processStructure} from "../query";
 import {isAxiosErrorRes} from "../../utils/http";
 import {getGreetingTime} from "../../utils/utils";
@@ -10,39 +8,10 @@ import config from "config";
 const Vod = Router();
 
 Vod.all("/", (req, res) => {
-    const delayed = new DelayedResponse(req, res);
-
-    delayed.on('done', function (results: any) {
-        // slowFunction responded within 5 seconds
-        res.send(results)
-        res.end();
-    });
-
-    delayed.on('error', function (err: Error) {
-        // handle error here
-        console.log("Error", err)
-        res.end();
-        // timeout will also raise an error since there is no "cancel" handler
-    });
-
-    delayed.on('abort', function (err: Error) {
-        // handle client disconnection
-        console.log("Client disconnected")
-        res.end();
-    });
-
-    ((respond: any) => {
-        console.log(respond);
-        setTimeout(function () {
-            // DelayedResponse.end(null, {id: 1})
-            respond(undefined, "Sent Then");
-        }, 10000);
-        return;
-    })(delayed.wait())
+    res.send("Reached")
 });
 
 Vod.post("/report", async (req, res) => {
-
 
     let {from, to, relative} = req.body;
     let {zone} = config.get("Time");
@@ -53,40 +22,16 @@ Vod.post("/report", async (req, res) => {
         res.end();
         return;
     }
+    let startDate = DateTime.fromMillis(Number.parseInt(from), {zone}).startOf("day");
+    let start = startDate.toMillis();
+    let endDate = DateTime.fromMillis(Number.parseInt(to), {zone}).endOf("day");
+    let end = endDate.toMillis();
 
-    const delayed = new DelayedResponse(req, res);
+    console.log(startDate.toSQL(), endDate.toSQL(), startDate.zoneName);
 
-    delayed.on('done', function (results: any) {
-        // slowFunction responded within 5 seconds
-        res.send(results)
-        res.end();
-    });
+    // let sql = `select vods_id as __value, vods_name as __text from vods`;
 
-    delayed.on('error', function (err: Error) {
-        // handle error here
-        console.log("Error", err)
-        res.end();
-        // timeout will also raise an error since there is no "cancel" handler
-    });
-
-    delayed.on('abort', function (err: Error) {
-        // handle client disconnection
-        console.log("Client disconnected")
-        res.end();
-    });
-
-    await (async (respond: any) => {
-
-        let startDate = DateTime.fromMillis(Number.parseInt(from), {zone}).startOf("day");
-        let start = startDate.toMillis();
-        let endDate = DateTime.fromMillis(Number.parseInt(to), {zone}).endOf("day");
-        let end = endDate.toMillis();
-
-        console.log(startDate.toSQL(), endDate.toSQL(), startDate.zoneName);
-
-        // let sql = `select vods_id as __value, vods_name as __text from vods`;
-
-        let sql = `SELECT 
+    let sql = `SELECT 
                 customers_login as 'Login',
                 profiles_name as 'Profile',
                 vods_name as 'VOD',
@@ -103,28 +48,33 @@ Vod.post("/report", async (req, res) => {
                 left join genres on vods_genres_genres_id = genres_id
                 where continue_watching_vods_updated > $__timeFrom() and continue_watching_vods_updated < $__timeTo()`;
 
-        try {
-            let {data} = await processSql(start, end, sql);
-            let structure = processStructure(data);
+    try {
+        let {data} = await processSql(start, end, sql);
+        let structure = processStructure(data);
 
-            let structureWithDate = structure.map((value) => {
-                const lastSeen = DateTime.fromISO(value['lastSeen'], {zone})
-                value['hour'] = lastSeen.hour;
-                value['timeOfDay'] = getGreetingTime(lastSeen.hour)
-                value['day'] = lastSeen.toFormat("ccc")
-                return value;
-            })
+        let structureWithDate = structure.map((value) => {
+            const lastSeen = DateTime.fromISO(value['lastSeen'], {zone})
+            value['hour'] = lastSeen.hour;
+            value['timeOfDay'] = getGreetingTime(lastSeen.hour)
+            value['day'] = lastSeen.toFormat("ccc")
+            return value;
+        })
 
-            respond(undefined, structureWithDate);
-        } catch (error) {
-            if (error.isAxiosError) {
-                isAxiosErrorRes(error, res)
-            }
-            console.log(error)
-            res.status(403).send("Could not process");
-            res.end()
+        res.send(structureWithDate)
+        res.end();
+    } catch (error) {
+        // handle error here
+        console.log("Error", error)
+        res.end();
+        // timeout will also raise an error since there is no "cancel" handler
+
+        if (error.isAxiosError) {
+            isAxiosErrorRes(error, res)
         }
-    })(delayed.wait())
+        console.log(error)
+        res.status(403).send("Could not process");
+        res.end()
+    }
 });
 
 export default Vod;
